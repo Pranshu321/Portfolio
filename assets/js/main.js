42a893a73d9c1bd184f867861ad80d0509d846ac
Pranshu321 <pranshujain0111@gmail.com>
2022-06-28 18:22:05 +0530

    PWA added

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,6 +13,20 @@
     }
   }
 
+  window.addEventListener('load' , ()=>{
+    registerSW();
+  });
+
+  async function registerSW(){
+    if('serviceWorker' in navigator){
+      try{
+        await navigator.serviceWorker.register('./serviceworker.js');
+      } catch(e){
+        console.log(`SW Registration failed`);
+      }
+    }
+  }
+
   
   //  * Easy event listener function
 
